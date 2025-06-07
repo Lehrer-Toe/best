579c1af0b477d45f2bc843d7afe13204953194b9
Lehrer-Toe <thomas.toellner@gmx.de>
2025-06-08 00:01:53 +0200

    Delete css directory

--- css/firebase-config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyDa24dH8TfAjEDzLPFjabY4gk3xBuFdFAw",
-  authDomain: "best-rsbs.firebaseapp.com",
-  databaseURL: "https://best-rsbs-default-rtdb.europe-west1.firebasedatabase.app/",
-  projectId: "best-rsbs",
-  storageBucket: "best-rsbs.appspot.com",
-  messagingSenderId: "42086270793",
-  appId: "1:42086270793:web:d53c731c40c82720213c98"
-};
